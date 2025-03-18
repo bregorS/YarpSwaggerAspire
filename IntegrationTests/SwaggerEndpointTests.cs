c986a3b97deaaa1f687595d40a620058ce4f8ea1
Roger Brooks <r.brooks@blackfinch.com>
2025-03-18 12:00:29 +0000

    Tidy tests

--- IntegrationTests/SwaggerEndpointTests.cs
+++ IntegrationTests/SwaggerEndpointTests.cs
@@ -1,90 +1,71 @@
-namespace IntegrationTests
+namespace IntegrationTests;
+
+[TestClass]
+public class SwaggerEndpointTests
 {
-    [TestClass]
-    public class SwaggerEndpointTests
+    [TestMethod]
+    public async Task GetSwaggerResourcesFromGatewayReturnsOkStatusCode()
     {
-        [TestMethod]
-        public async Task GetSwaggerResourcesFromGatewayReturnsOkStatusCode()
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YarpSwaggerTest_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
-            // Arrange
-            var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YarpSwaggerTest_AppHost>();
-            appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-            {
-                clientBuilder.AddStandardResilienceHandler();
-            });
-            await using var app = await appHost.BuildAsync();
-            var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-            await app.StartAsync();
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
 
-            // Act
-            var httpClientCommands = app.CreateHttpClient("commands");
-            await resourceNotificationService.WaitForResourceAsync("commands", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        // Act
+        var httpClientGateway = app.CreateHttpClient("gateway");
+        await resourceNotificationService.WaitForResourceAsync("gateway", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
 
-            var httpClientQueries = app.CreateHttpClient("queries");
-            await resourceNotificationService.WaitForResourceAsync("queries", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        // Assert
+        var response = await httpClientGateway.GetAsync("/swagger/index.html");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
 
-            var httpClientGateway = app.CreateHttpClient("gateway");
-            await resourceNotificationService.WaitForResourceAsync("gateway", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
-            // Assert
-            var response = await httpClientGateway.GetAsync("/swagger/index.html");
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-        }
-
-        [TestMethod]
-        public async Task GetSwaggerResourcesFromQueryApiReturnsOkStatusCode()
+    [TestMethod]
+    public async Task GetSwaggerResourcesFromQueryApiReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YarpSwaggerTest_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
-            // Arrange
-            var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YarpSwaggerTest_AppHost>();
-            appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-            {
-                clientBuilder.AddStandardResilienceHandler();
-            });
-            await using var app = await appHost.BuildAsync();
-            var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-            await app.StartAsync();
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
 
-            // Act
-            var httpClientCommands = app.CreateHttpClient("commands");
-            await resourceNotificationService.WaitForResourceAsync("commands", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        // Act
+        var httpClientQueries = app.CreateHttpClient("queries");
+        await resourceNotificationService.WaitForResourceAsync("queries", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
 
-            var httpClientQueries = app.CreateHttpClient("queries");
-            await resourceNotificationService.WaitForResourceAsync("queries", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        // Assert
+        var response = await httpClientQueries.GetAsync("/swagger/index.html");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+    }
 
-            var httpClientGateway = app.CreateHttpClient("gateway");
-            await resourceNotificationService.WaitForResourceAsync("gateway", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
-            // Assert
-            var response = await httpClientQueries.GetAsync("/swagger/index.html");
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-        }
-
-        [TestMethod]
-        public async Task GetSwaggerResourcesFromCommandApiReturnsOkStatusCode()
+    [TestMethod]
+    public async Task GetSwaggerResourcesFromCommandApiReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YarpSwaggerTest_AppHost>();
+        appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
         {
-            // Arrange
-            var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.YarpSwaggerTest_AppHost>();
-            appHost.Services.ConfigureHttpClientDefaults(clientBuilder =>
-            {
-                clientBuilder.AddStandardResilienceHandler();
-            });
-            await using var app = await appHost.BuildAsync();
-            var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
-            await app.StartAsync();
+            clientBuilder.AddStandardResilienceHandler();
+        });
+        await using var app = await appHost.BuildAsync();
+        var resourceNotificationService = app.Services.GetRequiredService<ResourceNotificationService>();
+        await app.StartAsync();
 
-            // Act
-            var httpClientCommands = app.CreateHttpClient("commands");
-            await resourceNotificationService.WaitForResourceAsync("commands", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
+        // Act
+        var httpClientCommands = app.CreateHttpClient("commands");
+        await resourceNotificationService.WaitForResourceAsync("commands", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
 
-            var httpClientQueries = app.CreateHttpClient("queries");
-            await resourceNotificationService.WaitForResourceAsync("queries", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
-            var httpClientGateway = app.CreateHttpClient("gateway");
-            await resourceNotificationService.WaitForResourceAsync("gateway", KnownResourceStates.Running).WaitAsync(TimeSpan.FromSeconds(30));
-
-            // Assert
-            var response = await httpClientCommands.GetAsync("/swagger/index.html");
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-        }
+        // Assert
+        var response = await httpClientCommands.GetAsync("/swagger/index.html");
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
     }
 }
